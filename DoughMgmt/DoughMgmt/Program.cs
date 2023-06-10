fcedc844437c13984802dcb1beaeb3204292512c
James Dougherty <jjdougherty89@gmail.com>
2023-06-09 21:34:58 -0400

    First commit

---
+++ DoughMgmt/DoughMgmt/Program.cs
@@ -0,0 +1,21 @@
+﻿using RestSharp;
+using System;
+
+namespace DoughMgmt
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Calling Rest API meow ...");
+
+            var client = new RestClient("https://swapi.dev/api/");
+            var request = new RestRequest("people/1", Method.Get);
+            request.AddHeader("Content-Type", "application/json");
+            RestResponse response = client.Execute(request);
+
+            Console.WriteLine("Response Status from API: " + response.StatusCode);
+            Console.WriteLine("Response from API: " + response.Content);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Dough-Mgmt
+
+## About
+
+- C Sharp application to manage API calls, Database storage, utilities, etc
